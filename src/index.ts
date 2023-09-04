96b391cce56817f59b6241f58c578e8bb51b54a0
SteinGaming <danialdaryab@gmail.com>
2023-09-04 22:34:22 +0200

    feat: simple endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-// @ts-ignore
 import express from "express";
 
-const PORT = process.env.TIMETWEAKER_PORT
+const PORT = process.env.TIMETWEAKER_PORT == undefined ? 30000 : process.env.TIMETWEAKER_PORT
 const app = express()
 
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+app.listen(PORT, () => {
+    console.log("Started at " + PORT + "!")
+})
