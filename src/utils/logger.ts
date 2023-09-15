5396d89dceeea45046d4d9b97347445ef905c3fd
SteinGaming <danialdaryab@gmail.com>
2023-09-15 16:00:08 +0200

    feat: dynamic logger leveling

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,29 +5,51 @@ dotenv.config()
 let env = process.env
 
 export default class Logger {
-    private name:string
-    constructor(name: string) {
-        this.name = name;
-    }
-    
-    
-    public log(level:string, colorToUse: any, message?: any, ...optionalParams: any[]) {
-        console.log(colorToUse(`[${this.name}] ${level} ->`), message, ...optionalParams);
+    private readonly name: string
+    public readonly level: string
+
+    constructor(name: string, level: string = env.LOGGER_LEVEL || "info") {
+        this.name = name
+        this.level = level
+
+        this.initialize(level)
     }
 
-    public info(message?: any, ...optionalParams: any[]) {
+    private initialize(level: string) {
+        const i = {
+            INFO: 1,
+            WARN: 2,
+            ERROR: 3,
+            DEBUG: 4,
+        }[level.toUpperCase().trim()] || -1
+
+        if (i > 0) this.info = this._info
+        if (i > 1) this.warn = this._warn
+        if (i > 2) this.error = this._error
+        if (i > 3) this.debug = this._debug
+    }
+    
+    public log(level:string, colorToUse: any, message?: any, ...optionalParams: any[]) {
+        console.log(colorToUse(`[${this.name}] ${level} ->`), message, ...optionalParams)
+    }
+
+    info = function(message?: any, ...optionalParams: any[]) {}
+    private _info(message?: any, ...optionalParams: any[]) {
         this.log("INFO", colors.blue , message, ...optionalParams)
     }
 
-    public warn(message?: any, ...optionalParams: any[]) {
+    warn = function(message?: any, ...optionalParams: any[]) {}
+    private _warn(message?: any, ...optionalParams: any[]) {
         this.log("WARN", colors.yellow , message, ...optionalParams)
     }
 
-    public error(message?: any, ...optionalParams: any[]) {
+    error = function(message?: any, ...optionalParams: any[]) {}
+    private _error(message?: any, ...optionalParams: any[]) {
         this.log("ERROR", colors.red , message, ...optionalParams)
     }
 
-    public debug(message?: any, ...optionalParams: any[]) {
+    debug = function(message?: any, ...optionalParams: any[]) {}
+    private _debug(message?: any, ...optionalParams: any[]) {
         this.log("DEBUG", colors.yellow, message, ...optionalParams)
     }
 }
